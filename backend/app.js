266896d99f4c418e0f4d2d183e40782c90462219
NadiaMok <mokhirevanadezda@gmail.com>
2019-11-19 22:56:48 -0500

    changes to the package files

--- backend/app.js
+++ backend/app.js
@@ -32,7 +32,7 @@ app.use('/', express.static(path.join(__dirname, 'dist/gamer-lobby')));
 app.use('/api', playerRoute)
 
 // Create port
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 4000;
 const server = app.listen(port, () => {
   console.log('Connected to port ' + port)
 })
