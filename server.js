b09bd223e3d1d6b104ce56bc9e960378fd67e3c1
NadiaMok <mokhirevanadezda@gmail.com>
2019-11-19 23:10:15 -0500

    changes to the package files

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ let express = require('express'),
   mongoose = require('mongoose'),
   cors = require('cors'),
   bodyParser = require('body-parser'),
-  dataBaseConfig = require('backend/database/db');
+  dataBaseConfig = require('/backend/database/db');
 
 
 // Connecting mongoDB
