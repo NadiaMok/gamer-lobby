449a6a77bded9ebb819bfc473ea142c469170e2e
NadiaMok <mokhirevanadezda@gmail.com>
2019-11-21 13:00:34 -0500

    added new route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PlayerRankingsComponent } from './components/player-rankings/player-rankings.component';
 import { LoginComponent } from './components/login/login.component';
+import { ListPlayerComponent } from './components/list-player/list-player.component';
 
 const routes: Routes = [
-  { path: '/', pathMatch: 'full', redirectTo: 'player-rankings' },
+  { path: '', pathMatch: 'full', redirectTo: 'player-rankings' },
   { path: 'player-rankings', component: PlayerRankingsComponent },
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'list-player', component: ListPlayerComponent }
 ];
 
 @NgModule({
