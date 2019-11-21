c03d2ee3b1f572606a801328549cebe6da7679c9
Parisa <parisa.khataei@georgebrown.ca>
2019-11-21 15:21:08 -0500

    whatever

---
+++ backend/model/Games.js
@@ -0,0 +1,15 @@
+var GAMES = [
+    "Bioshock",
+    "Amnesia: The Dark Descent",
+    "Tomb Raider",
+    "Fallout NV",
+    "Fallout 4",
+    "Portal",
+    "Portal 2",
+    "Super Mario",
+    "Grand Theft Auto V",
+    "Dead Space",
+    "Tetris"
+]
+
+export { GAMES };
